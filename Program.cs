2a920586560456000ab8e726ba12b63afd0b7cb4
rizwan4701798 <rizwan.farooq85@gmail.com>
2021-08-30 05:18:20 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -179,6 +179,11 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
 
+
+            SieveOfEratosthenes(50);
+
+
+
             int[][] jagged_arr =
   {
     new int[] {1, 2, 3, 4,7,8},
@@ -189,6 +194,9 @@ namespace TestConsoleApp
     new int[] {1, 2, 3, 4,7,8}
 };
 
+
+
+      
 
             int n1 = hourglassSum(jagged_arr);
             int[] staticIntArray12 = new int[7] { 200, -20, -20, -20, -20, -20, -20 };
@@ -406,6 +414,35 @@ namespace TestConsoleApp
             }
             
             return maxSum; 
+        }
+
+        public static void SieveOfEratosthenes(int n)
+        {
+
+      
+            bool[] prime = new bool[n + 1];
+
+            for (int i = 0; i < n; i++)
+                prime[i] = true;
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                // If prime[p] is not changed,
+                // then it is a prime
+                if (prime[p] == true)
+                {
+                    // Update all multiples of p
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+
+            // Print all prime numbers
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i] == true)
+                    Console.Write(i + " ");
+            }
         }
 
         // Complete the sockMerchant function below.
